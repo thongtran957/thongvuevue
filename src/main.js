07679860aac2d9ddf20822efb2711bb90fb156d3
thong <baibuvietnam@gmail.com>
2019-08-26 16:31:17 +0700

    asd

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import vueCustomElement from 'vue-custom-element'
+import 'document-register-element/build/document-register-element'
+Vue.use(vueCustomElement)
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.customElement('vue-widget', App)
